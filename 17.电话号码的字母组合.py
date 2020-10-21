0f161f8b324f336c3588a4384e23914d195dde44
xudong_yang <xudong_yang@intsig.net>
2020-10-21 14:53:16 +0800

    add new

--- 17.电话号码的字母组合.py
+++ 17.电话号码的字母组合.py
@@ -24,20 +24,56 @@ class Solution:
             "9": "wxyz",
         }
 
-        ans = []
-        for i, digit in enumerate(digits):
-            letters = phoneMap[digit]
-            if i == 0:
-                for letter in letters:
-                    ans.append(letter)
-            else:
-                new_list = []
-                for letter in letters:
-                    for each in ans:
-                        new_list.append(each + letter)
-                ans = new_list
+        length = len(digits)
 
-        return ans
+        def back(index):
+            if index == length:
+                ans_list.append("".join(tmp_list))
+                return
+            letters = phoneMap[digits[index]]
+            for letter in letters:
+                tmp_list.append(letter)
+                back(index+1)
+                tmp_list.pop()
+
+        tmp_list = []
+        ans_list = []
+        back(0)
+
+        return ans_list
+
+# Tips: traverse
+
+# class Solution:
+    # def letterCombinations(self, digits: str) -> List[str]:
+        # if not digits:
+            # return list()
+
+        # phoneMap = {
+            # "2": "abc",
+            # "3": "def",
+            # "4": "ghi",
+            # "5": "jkl",
+            # "6": "mno",
+            # "7": "pqrs",
+            # "8": "tuv",
+            # "9": "wxyz",
+        # }
+
+        # ans = []
+        # for i, digit in enumerate(digits):
+            # letters = phoneMap[digit]
+            # if i == 0:
+                # for letter in letters:
+                    # ans.append(letter)
+            # else:
+                # tmp_list = []
+                # for letter in letters:
+                    # for each in ans:
+                        # tmp_list.append(each + letter)
+                # ans = tmp_list
+
+        # return ans
 
 # @lc code=end
 
