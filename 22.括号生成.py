24d2b08d51e314456f3b7e30a29e6a60bf44a5ac
xudong_yang <xudong_yang@intsig.net>
2020-10-23 15:15:39 +0800

    add 22

---
+++ 22.括号生成.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+
+# Tips: backtracking
+
+class Solution:
+    def generateParenthesis(self, n: int):
+        def back(left, right, path):
+            if left == 0 and right == 0:
+                ans.append(path)
+                return
+            if left > 0:
+                back(left - 1, right, path + "(")
+            if left < right:
+                back(left, right - 1, path + ")")
+
+        ans = []
+        back(n, n, "")
+        return ans
+
+# @lc code=end
+
