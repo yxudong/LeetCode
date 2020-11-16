95e03b05a6a17ec9cd60ffa91a5a38c5c15bfbe0
yxudong <yxd18352839189@gmail.com>
2020-11-16 15:12:15 +0800

    add 283

---
+++ 283.移动零.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=283 lang=python3
+#
+# [283] 移动零
+#
+
+# @lc code=start
+
+#Tips： array, two-pointers
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 两个指针，j 在遇到元素为 0 时会停顿，i 继续向前走
+        # 遇到非 0 时，i 和 j 的元素交换，并且同时加 1，交换过后，j 指向的还是 0 的位置
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+                j = j + 1
+
+        return
+
+# @lc code=end
+
