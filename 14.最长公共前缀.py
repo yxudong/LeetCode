a9abbb01bafacafb238146fc911dfc0531cf2a5b
yxudong <yxd18352839189@gmail.com>
2020-11-26 10:51:57 +0800

    add 14

---
+++ 14.最长公共前缀.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=14 lang=python3
+#
+# [14] 最长公共前缀
+#
+
+# @lc code=start
+
+#Tips: string
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        min_len = min(len(each_str) for each_str in strs)
+        pre = ""
+        for i in range(0, min_len):
+            c = strs[0][i]
+            for each_str in strs:
+                if c != each_str[i]:
+                    return pre
+            pre = pre + c
+
+        return pre
+
+# @lc code=end
+
